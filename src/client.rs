c61b00824116730c1266dd5eced962c3ada95529
stevenlee <stevenl4@andrew.cmu.edu>
2024-01-03 00:47:16 -0500

    Start client functionality

--- src/client.rs
+++ src/client.rs
@@ -1,38 +1,95 @@
 //! Client implementation for Sink.
 //! 
+
+use std::io;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::str;
 
 const BUF_SIZE: usize = 8192;
 
+/// Represents a SinkClient.
+/// Syncs files from remote on specific address/port.
 pub struct SinkClient {
+    /// Address of server.
     addr: String,
+    /// Target sync directory.
     target_dir: String,
+    /// Receiving buffer.
     recv_buf: [u8; BUF_SIZE],
+    /// Sending buffer.
     send_buf: [u8; BUF_SIZE],
+    /// Command buffer.
+    command: String,
 }
 
 impl SinkClient {
+    /// Creates new SinkClient.
     pub fn new(addr: String, target_dir: String) -> SinkClient {
         SinkClient {
             addr,
             target_dir,
             recv_buf: [0; BUF_SIZE],
             send_buf: [0; BUF_SIZE],
+            command: String::new(),
         }
     }
 
+    /// Starts operation of SinkClient.
     pub fn start(&mut self) {
-        let mut stream : TcpStream = self.connect_address();
+        let stream : TcpStream = self.connect_address(); 
 
-        let bytes_sent = match stream.write(b"Hellooo\n") {
-            Ok(bytes_sent) => bytes_sent,
-            Err(error) => panic!("write error: {:?}", error),
-        };
-
-        println!("Bytes sent: {}", bytes_sent);
+        // TODO: Separate execution into different threads       
+        loop {
+            match self.prompt() {
+                Ok(size) => self.send_command(&stream, size),
+                Err(_error) => println!("sink: error parsing command"),
+            }
+            match self.read(&stream) {
+                Ok(_size) => {
+                    let message = match str::from_utf8(&self.recv_buf) {
+                        Ok(message) => message.to_string(),
+                        Err(error) => format!("received invalid utf8 sequence: {:?}", error),
+                    };
+                    // TODO: Parse server response
+                    println!("Message: {}", message);
+                },
+                Err(_error) => {},
+            }
+            // TODO: Handle writes
+            match self.write(&stream) {
+                Ok(_size) => {},
+                Err(_error) => {},
+            }
+        }
     }
 
+    /// Send command from command buffer.
+    fn send_command(&mut self, mut stream: &TcpStream, size: usize) {
+        match stream.write_all(&self.command.as_bytes()[0..size]) {
+            Ok(()) => {},
+            Err(error) => println!("sink: error sending command: {:?}", error),
+        }
+    } 
+
+    /// Write user input into command buffer.
+    fn prompt(&mut self) -> io::Result<usize> {
+        self.command.clear(); 
+        println!("sink>");
+        io::stdin().read_line(&mut self.command)
+    }
+
+    /// Read and place into receiving buffer.
+    fn read(&mut self, mut stream: &TcpStream) -> io::Result<usize> {
+        stream.read(&mut self.recv_buf)
+    }
+
+    /// Write from sending buffer.
+    fn write(&mut self, mut stream: &TcpStream) -> io::Result<usize> {
+        stream.write(&mut self.send_buf)
+    }
+
+    /// Connect SinkClient to specified address.
     fn connect_address(&mut self) -> TcpStream {
         match TcpStream::connect(&self.addr) {
             Ok(stream) => {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+//! A Simple File Syncing Utility.
 //! NOTE: Most planned features have not been implemented yet.
-//! Sink: A Simple File Syncing Utility
 //! 
 //! Provides a simple cli interface for syncing files between local and remote
 //! directories.
@@ -25,49 +25,45 @@ struct Args {
     server: Option<Vec<String>>, 
 }
 
+fn start_server(args : Args) {
+    match args.server {
+        Some(s_args) => {
+            let mut server = SinkServer::new(
+                s_args[0].clone(), 
+                s_args[1].clone(),
+            );
+            server.start();
+        },
+        None => {},
+    };
+}
+
+fn start_client(args: Args) {
+    match args.client {
+        Some(c_args) => {
+            let mut client = SinkClient::new(
+                c_args[0].clone(),
+                c_args[1].clone(),
+            );
+            client.start();
+        }
+        None => {},
+    };
+}
+
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    // TODO: Refactor
-    let is_server: bool = match args.client {
-        Some(_) => {
-            match args.server {
-                Some(_) => panic!("expected only one of --client, --server"),
-                None => false,
-            }
-        },
-        None => {
-            match args.server {
-                Some(_) => true,
-                None => panic!("expected one of --client, --server"),
-            }
-        }
-    };
+    if !(args.client.is_some() ^ args.server.is_some()) {
+        panic!("expected one of --client, --server")
+    }
 
-    if is_server {
-        let s_args: Vec<String> = match args.server {
-            Some(s_args) => s_args,
-            None => panic!("expected arguments for --server"),
-        };
-
-        let mut server = SinkServer::new(
-            s_args[0].clone(), 
-            s_args[1].clone(),
-        );
-        server.start();
+    if args.client.is_some() {
+        start_client(args);
     }
     else {
-        let c_args: Vec<String> = match args.client {
-            Some(c_args) => c_args,
-            None => panic!("expected arguments for --client"),
-        };
-
-        let mut client = SinkClient::new(
-            c_args[0].clone(),
-            c_args[1].clone(),
-        );
-        client.start();
+        start_server(args);
     }
-    
+
     Ok(())
 }
